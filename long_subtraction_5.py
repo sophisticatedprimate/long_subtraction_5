e76c8829fbfc9e5a422c466cecbb26d93847f3a6
SophisticatedPrimate <admin@sophisticatedprimate.com>
2022-09-19 19:06:38 -0500

    Add files via upload

---
+++ long_subtraction_5.py
@@ -0,0 +1,167 @@
+"""
+Check out the official python docs to learn more about python programming!
+https://docs.python.org/3/
+
+Refer to long_subtraction_1.py for further information on subtracting values in python:
+https://github.com/sophisticatedprimate/long_subtraction_1/blob/main/long_subtraction_1.py
+
+To run the program: 
+    1. Open a terminal on a computer with python 3 installed
+    2. Navigate to a directory containing this file (long_subtraction_5.py)
+    3. Run the program with the following command: python3 long_subtraction_5.py
+    4. A print out of the sums should appear after the cursor
+
+If error:
+    a. make sure python is installed (run: python3 --version)
+    b. make sure you have the file installed and in the current directory
+    c. if you are still having issues or see any general issues in the programming please report using the contact information at end of file.
+"""
+
+# Problem 1 
+difference = 8453 - 6292  
+print("{0:<4} - {1:<4} = {2:<4}".format(8453,6292,difference)) 
+
+# Problem 2 
+difference = 7003 - 1421  
+print("{0:<4} - {1:<4} = {2:<4}".format(7003,1421,difference)) 
+
+# Problem 3 
+difference = 6352 - 5079  
+print("{0:<4} - {1:<4} = {2:<4}".format(6352,5079,difference)) 
+
+# Problem 4 
+difference = 8154 - 2781  
+print("{0:<4} - {1:<4} = {2:<4}".format(8154,2781,difference)) 
+
+# Problem 5 
+difference = 4652 - 2450  
+print("{0:<4} - {1:<4} = {2:<4}".format(4652,2450,difference)) 
+
+# Problem 6 
+difference = 5579 - 5372  
+print("{0:<4} - {1:<4} = {2:<4}".format(5579,5372,difference)) 
+
+# Problem 7 
+difference = 9059 - 2492  
+print("{0:<4} - {1:<4} = {2:<4}".format(9059,2492,difference)) 
+
+# Problem 8 
+difference = 4441 - 3152  
+print("{0:<4} - {1:<4} = {2:<4}".format(4441,3152,difference)) 
+
+# Problem 9 
+difference = 7282 - 1766  
+print("{0:<4} - {1:<4} = {2:<4}".format(7282,1766,difference)) 
+
+# Problem 10
+difference = 9824 - 3443  
+print("{0:<4} - {1:<4} = {2:<4}".format(9824,3443,difference)) 
+
+# Problem 11
+difference = 4813 - 2594  
+print("{0:<4} - {1:<4} = {2:<4}".format(4813,2594,difference)) 
+
+# Problem 12
+difference = 8990 - 3482  
+print("{0:<4} - {1:<4} = {2:<4}".format(8990,3482,difference)) 
+
+# Problem 13
+difference = 9732 - 9432  
+print("{0:<4} - {1:<4} = {2:<4}".format(9732,9432,difference)) 
+
+# Problem 14
+difference = 8394 - 194  
+print("{0:<4} - {1:<4} = {2:<4}".format(8394,194,difference)) 
+
+# Problem 15
+difference = 4830 - 2606  
+print("{0:<4} - {1:<4} = {2:<4}".format(4830,2606,difference)) 
+
+# Problem 16
+difference = 5309 - 4716  
+print("{0:<4} - {1:<4} = {2:<4}".format(5309,4716,difference)) 
+
+# Problem 17
+difference = 8648 - 3454  
+print("{0:<4} - {1:<4} = {2:<4}".format(8648,3454,difference)) 
+
+# Problem 18
+difference = 5228 - 2885  
+print("{0:<4} - {1:<4} = {2:<4}".format(5228,2885,difference)) 
+
+# Problem 19
+difference = 7846 - 2433  
+print("{0:<4} - {1:<4} = {2:<4}".format(7846,2433,difference)) 
+
+# Problem 20
+difference = 9364 - 8719  
+print("{0:<4} - {1:<4} = {2:<4}".format(9364,8719,difference)) 
+
+# Problem 21
+difference = 9449 - 3926  
+print("{0:<4} - {1:<4} = {2:<4}".format(9449,3926,difference)) 
+
+# Problem 22
+difference = 5757 - 5299  
+print("{0:<4} - {1:<4} = {2:<4}".format(5757,5299,difference)) 
+
+# Problem 23
+difference = 8590 - 6785  
+print("{0:<4} - {1:<4} = {2:<4}".format(8590,6785,difference)) 
+
+# Problem 24
+difference = 7885 - 5155  
+print("{0:<4} - {1:<4} = {2:<4}".format(7885,5155,difference)) 
+
+# Problem 25
+difference = 9374 - 8021  
+print("{0:<4} - {1:<4} = {2:<4}".format(9374,8021,difference)) 
+
+# Problem 26
+difference = 8603 - 3378  
+print("{0:<4} - {1:<4} = {2:<4}".format(8603,3378,difference)) 
+
+# Problem 27
+difference = 9769 - 217  
+print("{0:<4} - {1:<4} = {2:<4}".format(9769,217,difference)) 
+
+# Problem 28
+difference = 7872 - 7642  
+print("{0:<4} - {1:<4} = {2:<4}".format(7872,7642,difference)) 
+
+# Problem 29
+difference = 5301 - 4630  
+print("{0:<4} - {1:<4} = {2:<4}".format(5301,4630,difference)) 
+
+# Problem 30
+difference = 9882 - 4006  
+print("{0:<4} - {1:<4} = {2:<4}".format(9882,4006,difference)) 
+
+# Problem 31
+difference = 7350 - 2674  
+print("{0:<4} - {1:<4} = {2:<4}".format(7350,2674,difference)) 
+
+# Problem 32
+difference = 6725 - 5450  
+print("{0:<4} - {1:<4} = {2:<4}".format(6725,5450,difference)) 
+
+# Problem 33
+difference = 9463 - 7842  
+print("{0:<4} - {1:<4} = {2:<4}".format(9463,7842,difference)) 
+
+# Problem 34
+difference = 4735 - 4227  
+print("{0:<4} - {1:<4} = {2:<4}".format(4735,4227,difference)) 
+
+# Problem 35
+difference = 8802 - 6068  
+print("{0:<4} - {1:<4} = {2:<4}".format(8802,6068,difference)) 
+
+"""
+Contact information
+
+General Queries: admin@sophisticatedprimate.com
+Report errors, bugs, and problems at:
+    email:  bug@sophisticatedprimate.com
+    github: https://github.com/sophisticatedprimate/long_subtraction_5/issues
+"""
